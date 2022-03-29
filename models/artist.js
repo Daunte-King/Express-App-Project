abe50128f363a12321eb480e75870a2004802109
Daunte-King <dauntedking@gmail.com>
2022-03-29 12:56:32 -0600

    adding user model

--- models/artist.js
+++ models/artist.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
+const artistSchema = new Schema ({
+    artist: String,
+    decade: String,
+
+})
+
+module.exports = mongoose.model('Artist', artistSchema);
