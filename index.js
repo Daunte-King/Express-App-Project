193e1a619e7f2f7de9e462de092dc529213393b0
Daunte-King <dauntedking@gmail.com>
2022-03-29 12:06:39 -0600

    Hoisting App

---
+++ index.js
@@ -0,0 +1,21 @@
+//index.js
+
+//Require external Modules
+const express = require('express');
+const path = require('path');
+
+//App Variables
+const app = express();
+const port = process.env.PORT || "8000";
+
+//App Configuration
+
+//Routes Definitions
+app.get("/", (req, res) => {
+    res.status(200).send("Who's Your GOAT")
+});
+
+//Server Activation
+app.listen(port, () => {
+    console.log('Listening to requests on http://localhost:${port}');
+});
